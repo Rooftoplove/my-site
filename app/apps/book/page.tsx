9f06bbb707b9bf332e2acc68332958e7b61a4b5c
Rooftoplove <kamekazu910@yahoo.co.jp>
2026-04-19 16:48:35 +0900

    first commit

---
+++ app/apps/book/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <h1>本管理アプリページ</h1>
+}
---
+++ app/apps/kakeibo/page.tsx
@@ -0,0 +1,27 @@
+export default function Page() {
+    return (
+            <main className="max-w-3xl mx-auto p-6">
+            
+            <h1 className="text-3xl font-bold">家計簿アプリ</h1>
+            
+            <p className="mt-2 text-gray-600">
+            シンプルに使える家計簿アプリです
+            </p>
+            
+            <h2 className="mt-6 font-bold">特徴</h2>
+            <ul className="list-disc ml-5 mt-2">
+            <li>簡単入力</li>
+            <li>カテゴリ管理</li>
+            <li>グラフで確認</li>
+            </ul>
+            
+            <a
+            href="#"
+            className="block bg-blue-500 text-white text-center p-3 rounded-xl mt-6"
+            >
+            App Storeでダウンロード
+            </a>
+            
+            </main>
+            )
+}
