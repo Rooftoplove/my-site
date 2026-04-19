4468d60b45ef46a50778090d5fb30ef750227c80
Rooftoplove <kamekazu910@yahoo.co.jp>
2026-04-19 16:59:47 +0900

    update

--- app/apps/book/page.tsx
+++ app/apps/book/page.tsx
@@ -1,3 +1,27 @@
 export default function Page() {
-    return <h1>本管理アプリページ</h1>
+    return (
+            <main className="max-w-3xl mx-auto p-6">
+            
+            <h1 className="text-3xl font-bold">本管理アプリ</h1>
+            
+            <p className="mt-2 text-gray-600">
+            シンプルに使える本管理アプリです
+            </p>
+            
+            <h2 className="mt-6 font-bold">特徴</h2>
+            <ul className="list-disc ml-5 mt-2">
+            <li>簡単入力</li>
+            <li>カテゴリ管理</li>
+            <li>グラフで確認</li>
+            </ul>
+            
+            <a
+            href="#"
+            className="block bg-blue-500 text-white text-center p-3 rounded-xl mt-6"
+            >
+            App Storeでダウンロード
+            </a>
+            
+            </main>
+            )
 }
